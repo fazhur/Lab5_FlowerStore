4f10d1342a693e51fca5faba5320797d25bd4d90
fazhur <fedir.zhurba@ucu.edu.ua>
2022-10-24 18:00:49 +0300

    source code updated

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,18 @@
+package flower.store;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+public class Store {
+    @Getter
+    private ArrayList<FlowerBucket> buckets = new ArrayList<>();
+
+    public void addBucket(FlowerBucket bucket){
+        buckets.add(bucket);
+    }
+
+    public boolean searchBucket(FlowerBucket bucket){
+        return buckets.contains(bucket);
+    }
+}
